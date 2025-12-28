58b11f1402ae711f8b278356c6e1b8d54cd53143
servidor de MZXN <mauricio98932383@gmail.com>
2025-12-28 16:32:37 -0500

    Update mzxn-best.lua

--- mzxn-best.lua
+++ mzxn-best.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/af80809b4e125a04d6a839b89b7c2ea9dc48252d1d2b1799c18403a0276d1684/download"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/37b082cbdf77e07398494a1a7150fb40.lua"))()
